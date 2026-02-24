546cbe923e18c52169bb21fece2d17b3c300173f
adikc015 <153607289+adikc015@users.noreply.github.com>
2026-02-24 11:57:10 +0530

    Update database password for connection

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, text
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = "root"
-DB_PASSWORD = "jaya@123"
+DB_PASSWORD = "Adi12345"
 DB_HOST = "localhost"
 DB_PORT = "3306"
 DB_NAME = "project1"
